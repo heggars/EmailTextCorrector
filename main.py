43b1c11c35d248876c8441a5e65db8b0636f4780
heggars <grangehermitage1976@gmail.com>
2023-06-05 23:19:22 +0400

    Commit 2

---
+++ main.py
@@ -0,0 +1,111 @@
+# This is a sample Python script.
+import streamlit as st
+from langchain import PromptTemplate
+from langchain.llms import OpenAI
+import openai
+import os
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+myAPIKey = openai.api_key
+
+template = """
+    Below is an email that may be poorly worded and structured.
+    Your goal is to:
+    - Format the email professionally
+    - Convert the input text to a specified tone
+    - Convert the input text to a specified dialect
+
+    Here are some examples different Tones:
+    - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
+    - Informal: Went to Barcelona for the weekend. Lots to tell you.  
+
+    Here are some examples of words in different dialects:
+    - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
+    - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
+
+    Example Sentences from each dialect:
+    - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
+    - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
+
+    Please start the email with a warm introduction. Add the introduction if you need to.
+    Please end the email with an appropriate phrase
+    
+    Below is the email, tone, and dialect:
+    TONE: {tone}
+    DIALECT: {dialect}
+    EMAIL: {email}
+
+    YOUR {dialect} RESPONSE:
+"""
+prompt = PromptTemplate(
+    input_variables=["tone", "dialect", "email"],
+    template=template,
+)
+
+def load_LLM(openai_api_key):
+    """Logic for loading the chain you want to use should go here."""
+    # Make sure your openai_api_key is set as an environment variable
+    llm = OpenAI(temperature=.3, openai_api_key=openai_api_key)
+    return llm
+
+st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
+st.header("")
+
+hcol1, hcol2, hcol3 = st.columns([1,6,1])
+
+with hcol1:
+    st.write("")
+
+with hcol2:
+    st.image(image='LOLLogo.png', width=500, caption="This email corrector should help with your written english")
+
+with hcol3:
+    st.write("")
+
+st.markdown("## Enter Your Email To Convert")
+
+col1, col2 = st.columns(2)
+with col1:
+    option_tone = st.selectbox(
+        'Which tone would you like your email to have?',
+        ('Formal', 'Informal'))
+
+with col2:
+    option_dialect = st.selectbox(
+        'Which English dialect would you like?',
+        ('American', 'British'))
+
+
+def get_text():
+    input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...",
+                              key="email_input")
+    return input_text
+
+
+email_input = get_text()
+
+if len(email_input.split(" ")) > 700:
+    st.write("Please enter a shorter email. The maximum length is 700 words.")
+    st.stop()
+
+def update_text_with_example():
+    print ("in updated")
+    st.session_state.email_input = "Sally I am starts work at yours monday from dave"
+
+st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
+
+
+st.markdown("### Your Converted Email:")
+
+if email_input:
+    if not myAPIKey:
+        st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
+        st.stop()
+
+    llm = load_LLM(openai_api_key=myAPIKey)
+
+    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+
+    formatted_email = llm(prompt_with_email)
+
+    st.write(formatted_email)
